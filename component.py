28218bf6b1a5e8e7c4390b8c97cbed603063984d
Roy Tu <thatsnail@gmail.com>
2016-02-06 14:10:47 -0800

    Refactored...?

--- component.py
+++
@@ -1,8 +0,0 @@
-
-""" Contains the Component module. """
-
-class Component(object):
-    """ Components have vertices, each with their own voltages, and currents
-    along each wire, and constraints that must be satisfied.  They can also hold
-    internal state. """
-    pass
